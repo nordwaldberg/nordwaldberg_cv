e719588f8609f292298407b31fa0a8cd63510083
nordwaldberg <nord.waldberg@gmail.com>
2024-09-05 17:16:37 +0300

    Add Dropdown and download CVs feature

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import styles from './Header.module.scss';
-import {Brackets} from '../misc/components';
+import {Brackets, Dropdown} from '../misc/components';
 import {ThemeContext} from '../App/App';
 import OpenContactsModal from '../OpenContactsModal/OpenContactsModal';
 import ShowCertificatesModal from '../ShowCertificatesModal/ShowCertificatesModal';
@@ -11,21 +11,24 @@ const Header: React.FC = () => {
     const [isContactsVisible, setContactsVisible] = React.useState(false);
     const [isCertificatesVisible, setCertificatesVisible] = React.useState(false);
 
+    const dropdownOptions: React.ReactNode[] | React.ReactElement[] = [
+        <a href="/CV_EN.pdf" download="CV_EN.pdf">english</a>,
+        <a href="/CV_RU.pdf" download="CV_RU.pdf">russian</a>,
+    ];
+
     return (
         <div className={styles.header}>
             <a className={`${styles.logo} ${theme === 'dark' ? styles.dark : styles.light}`}></a>
             <ul className={styles.links}>
-                <li>
-                    <Brackets>
-                        downloadCV
-                    </Brackets>
+                <li className={styles.link}>
+                    <Dropdown btnName={'downloadCV'} options={dropdownOptions}/>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <Brackets>
                         <button onClick={() => setContactsVisible(true)}>openContacts</button>
                     </Brackets>
                 </li>
-                <li>
+                <li className={styles.link}>
                     <Brackets>
                         <button onClick={() => setCertificatesVisible(true)}>showCertificates</button>
                     </Brackets>
--- src/components/misc/components.tsx
+++ src/components/misc/components.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from './components.module.scss';
+import {ThemeContext} from '../App/App';
 
 interface BracketsProps {
     children?: React.ReactNode | React.ReactElement;
+}
+
+interface DropdownProps {
+    btnName: string;
+    options?: React.ReactNode[] | React.ReactElement[];
 }
 
 const Brackets: React.FC<BracketsProps> = ({children}) => {
     return (
         <div className={styles.brackets}>
             <span>[</span>
-                {children}
+            {children}
             <span>]</span>
         </div>
     );
@@ -30,7 +36,36 @@ const Marquee: React.FC = () => {
 
 }
 
+const Dropdown: React.FC<DropdownProps> = ({btnName, options}) => {
+    const [theme, setTheme] = useContext(ThemeContext);
+
+    const handleOptionClick = (option: string) => {
+        console.log(`Вы выбрали: ${option}`);
+    };
+
+
+    return (
+        <div className={styles.dropdown}>
+            <Brackets>
+                <button className={styles.dropdownBtn}>
+                    {btnName}
+                </button>
+            </Brackets>
+            <ul className={`${styles.dropdownOptions} ${styles[theme ? theme : 'dark']}`}>
+                <li className={`${styles.option} ${styles[theme ? theme : 'dark']}`} onClick={() => handleOptionClick('1')}>
+                    {options ? options[0] : null}
+                </li>
+                <li className={`${styles.option} ${styles[theme ? theme : 'dark']}`} onClick={() => handleOptionClick('2')}>
+                    {options ? options[1] : null}
+                </li>
+            </ul>
+
+        </div>
+    );
+}
+
 export {
     Brackets,
     Marquee,
+    Dropdown,
 };
